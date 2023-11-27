ee0b85057b9e591b1e5e30c5cc57d366ce25dd33
jakekato <loveillusion02@gmail.com>
2023-11-28 00:53:11 +0700

    Update: new add button

--- public/scripts/client-socket.js
+++ public/scripts/client-socket.js
@@ -1,12 +1,17 @@
-let socket = new WebSocket("ws://localhost:8080")
+var socket = new WebSocket("ws://localhost:8080")
 
 var isPlaying = false;
+var mode = 'search';
+var current_video = [];
+
+const pageURL = String(document.URL);
+const mainURL = pageURL.substring(0, nthIndex(pageURL, '/', 3));
+const playlistJSON = mainURL + '/storage';
 
 socket.onopen = function(e) {
     console.log("[open] Connection established");
+    close_loading_screen();
 };
-
-var mode = 'search';
 
 socket.onmessage = function(event) {
     console.log(event.data);
@@ -33,6 +38,7 @@ socket.onclose = function(event) {
     } else {
         //alert('[close] Connection died');
     }
+    open_loading_screen();
 };
 
 socket.onerror = function(error) {
@@ -47,19 +53,15 @@ function search() {
     var input = document.querySelector('[bar-field]').value;
     if (input.length > 0) {
         clear_list(); 
-        mode = 'search';
+        change_mode('queque');
         sendMsg('search', [input]);
     }
 }
 
-var current_video = [];
 function card_func(thumbnail, title, creator, url) {
-    const image_preview = document.querySelector('[img]');
-    image_preview.src = thumbnail;
-    console.log(title);
-    document.getElementById('myicon').className = "fa fa-pause"; 
     isPlaying = true;
-    current_video = [thumbnail, title, creator, url];
+    change_preview_image(thumbnail);
+    set_video(thumbnail, title, creator, url);
     if (url != null) {
         switch (mode) {
             case 'search':
@@ -68,76 +70,60 @@ function card_func(thumbnail, title, creator, url) {
             case 'queque':
                 sendMsg('queque', [url]);
                 break;
+            case 'playlist':
+                sendMSg('playlist', [url]);
+                break;
             case 'download':
                 break;
         }
     }
 }
 
+function change_mode(mode) { mode = mode; }
+
+function set_video(thumbnail, title, creator, url) {
+    current_video = [thumbnail, title, creator, url];
+}
+
 function toggle_state() {
-    const toggle_button = document.getElementById('myicon');
-    if (isPlaying == true) { 
-        isPlaying = false;
-        toggle_button.className = "fa fa-play";
-        sendMsg('pause', [true]);
-    } else {
-        isPlaying = true;
-        toggle_button.className = "fa fa-pause";
-        sendMsg('play', [true]);
-    }
+    play_button_toggle(isPlaying);
+    if (isPlaying) { sendMsg('pause', [true]); }
+    else { sendMsg('play', [true]); }
+    isPlaying = !(isPlaying);
 }
 
 function add_playlist() {
-    if (current_video.length > 0) {
-        sendMsg('add_playlist', current_video);
-    }
+    if (current_video.length > 0) { sendMsg('add_playlist', current_video); }
+}
+
+function add_queque() {
+    if (current_video.length > 0) { sendMsg('add_queque', current_video); }
+
+}
+
+function add_download() {
+    if (current_video.length > 0) { sendMsg('add_download', current_video); }
 }
 
 function clear_list() { videosList.innerHTML = ''; }
 
 function nthIndex(str, pat, n) {
-    var L = str.length,
-        i = -1;
-    while (n-- && i++ < L) {
-        i = str.indexOf(pat, i);
-        if (i < 0) break;
-    }
+    var L = str.length, i = -1;
+    while (n-- && i++ < L) { i = str.indexOf(pat, i); if (i < 0) break; }
     return i;
 }
-const pageURL = String(document.URL);
-const mainURL = pageURL.substring(0, nthIndex(pageURL, '/', 3));
-const playlistJSON = mainURL + '/storage';
 
 function load_queque() {
     clear_list();
-    mode = 'queque'; 
+    change_mode('queque');
     fetch(playlistJSON) 
-    .then( (response) => { 
-        var res = response; 
-        var json = res.json(); 
+    .then( (res) => { 
+        var raw = res; 
+        var json = raw.json(); 
         json.then( (res) => {
-            var obj = res;
-            obj['playlist'].forEach( (dict, index) => {
-                addCard(dict.thumbnail, dict.title, dict.creator, dict.url);
-            });
+            var json = res;
+            json['playlist'].forEach( (dict, index) => { add_card(dict.thumbnail, dict.title, dict.creator, dict.url); });
         })
     })
-    sidebar_close();
-}
-
-const videoCardTemplate = document.querySelector('[video-card-template]');
-const videosList = document.querySelector('[videos-list]');
-
-function addCard(link, title, creator, url) {
-    const card = videoCardTemplate.content.cloneNode(true).children[0];
-    const thumbnail = card.querySelector('[thumbnail]');
-    const videoTitle = card.querySelector('[video-title]');
-    const videoCreator = card.querySelector('[video-creator]');
-
-    thumbnail.src = link;
-    videoTitle.textContent = title;
-    videoCreator.textContent = creator;
-
-    videosList.append(card);
-    card.addEventListener("click", function(){card_func(link, title, creator, url);});
+    open_side_bar();
 }
--- public/scripts/index.js
+++ public/scripts/index.js
@@ -1,11 +1,63 @@
-var barOpen = false;
+const sidebar = document.querySelector("[side-bar]");
+const sidebar_overlay = document.querySelector("[side-bar-overlay]");
+const panel = document.querySelector("[bottom-panel]");
+const videoCardTemplate = document.querySelector('[video-card-template]');
+const videosList = document.querySelector('[videos-list]');
+const panel_overlay = document.querySelector("[bottom-panel-overlay]");
 
-function sidebar_open() {
-    document.querySelector('[side-bar]').style.display = 'block';
-    barOpen = true;
+function open_side_bar() {
+    sidebar.classList.toggle("active");
+    sidebar_overlay.classList.toggle("active");
 }
 
-function sidebar_close() {
-    document.querySelector('[side-bar]').style.display = 'none';
-    barOpen = false;
+function close_side_bar() {
+    sidebar.classList.remove("active");
+    sidebar_overlay.classList.remove("active");
+}
+
+function open_bottom_panel() {
+    panel.classList.toggle("active");
+    panel_overlay.classList.toggle("active");
+}
+
+function close_bottom_panel() {
+    panel.classList.remove("active");
+    panel_overlay.classList.remove("active");
+}
+
+function open_loading_screen() {
+}
+
+function close_loading_screen() {
+}
+
+document.onclick = function(e) {
+    if(e.target.id === "bottom-overlay") { close_bottom_panel() }
+    if(e.target.id === "side-bar-overlay") { close_side_bar() }
+}
+
+function  play_button_toggle(isPlaying) {
+    const toggle_button = document.getElementById('myicon');
+    if (isPlaying) { toggle_button.className = "fa fa-play"; }
+    else { toggle_button.className = "fa fa-pause"; }
+}
+
+function change_preview_image(thumbnail) {
+    const image_preview = document.querySelector('[img]');
+    image_preview.src = thumbnail;
+    document.getElementById('myicon').className = "fa fa-pause"; 
+}
+
+function add_card(link, title, creator, url) {
+    const card = videoCardTemplate.content.cloneNode(true).children[0];
+    const thumbnail = card.querySelector('[thumbnail]');
+    const videoTitle = card.querySelector('[video-title]');
+    const videoCreator = card.querySelector('[video-creator]');
+
+    thumbnail.src = link;
+    videoTitle.textContent = title;
+    videoCreator.textContent = creator;
+
+    videosList.append(card);
+    card.addEventListener("click", function(){card_func(link, title, creator, url);});
 }
